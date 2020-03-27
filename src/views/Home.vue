fe2c948d9c40cf0d75a7e0908437a71ce1591293
Uzir Thapa <uzir.thapa@kirkwood.edu>
2020-03-27 15:07:47 -0500

    added bouncing ball

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <c-game-canvas msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CGameCanvas from '@/components/CGameCanvas.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CGameCanvas
+  }
+}
+</script>
