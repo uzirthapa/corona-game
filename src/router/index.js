280cf50d666cb20009bae794c4743de97282061e
amartensen <adam.martensen@kirkwood.edu>
2020-03-29 16:29:16 -0500

    Merge remote-tracking branch 'origin/covid-1.0.0' into am-dev
    # Conflicts:
    #	src/App.vue
    #	src/views/Home.vue
    #	yarn.lock

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Game from "../views/Game.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter)
 
@@ -12,15 +13,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/',
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/game',
     name: 'Game',
     component: Game
   },
